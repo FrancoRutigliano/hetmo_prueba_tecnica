e54ed2bf44a5f50b39e58569825c9f5c6b174f99
FrancoRutigliano <francorutigliano11@gmail.com>
2024-09-24 23:23:04 -0300

    add env variables, godot and docker working well

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("err to load .env variables")
+	}
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("hello world")
 	})
 
-	app.Listen(":8080")
+	app.Listen(os.Getenv("PORT"))
 }
