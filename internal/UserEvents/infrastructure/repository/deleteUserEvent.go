46be11f75229f7707e580e41f3b0d2fe3a727de8
Franco Rutigliano <francorutigliano11@gmail.com>
2024-10-02 17:44:17 -0300

    delete userEvent

--- internal/UserEvents/infrastructure/repository/deleteUserEvent.go
+++ internal/UserEvents/infrastructure/repository/deleteUserEvent.go
@@ -1,7 +1,32 @@
 package infraUserEventsRepository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
 
 func (u *UserEventsImpl) DeleteUserEvent(userId, eventId string, db *sqlx.DB) error {
+	query := `
+	DELETE FROM "public".user_event 
+	WHERE 
+		user_id = $1 
+		AND 
+		event_id = $2;`
+
+	result, err := db.Exec(query, userId, eventId)
+	if err != nil {
+		return fmt.Errorf("error deleting user event: %s", err.Error())
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("error checking affected rows")
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("no event found with the provided ID")
+	}
+
 	return nil
 }
--- internal/UserEvents/pkg/useCases/useCasesImpl/deleteUserEvent.go
+++ internal/UserEvents/pkg/useCases/useCasesImpl/deleteUserEvent.go
@@ -1,7 +1,14 @@
 package userEventsUseCasesImpl
 
-import httpresponse "hetmo_prueba_tecnica/pkg/httpResponse"
+import (
+	httpresponse "hetmo_prueba_tecnica/pkg/httpResponse"
+	"net/http"
+)
 
-func (u *UserEvents) DeleteUserEvents(userID, eventId string) httpresponse.ApiResponse {
-	return *httpresponse.NewApiError(200, "delete", nil)
+func (u *UserEvents) DeleteUserEvents(userId, eventId string) httpresponse.ApiResponse {
+	err := u.UserEvents.Impl.DeleteUserEvent(userId, eventId, u.Db)
+	if err != nil {
+		return *httpresponse.NewApiError(http.StatusInternalServerError, err.Error(), nil)
+	}
+	return *httpresponse.NewApiError(http.StatusOK, "succesfully user deleted", nil)
 }
